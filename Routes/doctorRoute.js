20c303728251040922a14876f579716862609088
iris-credot <tetairiscredot@gmail.com>
2025-04-23 12:25:18 +0200

    updated notificationsss

--- Routes/doctorRoute.js
+++ Routes/doctorRoute.js
@@ -5,7 +5,7 @@ const authController = require('../Controllers/doctorController');
 
 
 Doctorrouter.post('/create',authController.createDoctor);
-Doctorrouter.get('/all' ,auth.adminJWT,authController.getAllDoctors);
+Doctorrouter.get('/all' ,auth.AuthJWT,authController.getAllDoctors);
 Doctorrouter.delete('/delete/:id',auth.adminJWT, authController.deleteDoctor);
 Doctorrouter.put('/put/:id', auth.BothJWT,authController.updateDoctor);
 Doctorrouter.get('/getdoctor/:id', auth.adminJWT,authController.getDoctorById);
