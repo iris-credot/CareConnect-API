e2086edf19d6ef0fe2d6ba04ab500bef00922a07
iris-credot <tetairiscredot@gmail.com>
2025-05-05 14:50:17 +0200

    worked on the rechedule of appointment

--- Routes/appointmentRoute.js
+++ Routes/appointmentRoute.js
@@ -11,7 +11,7 @@ Appointmentrouter.delete('/delete/:id', auth.adminJWT, appointmentController.del
 Appointmentrouter.get('/byPatient/:id', auth.AuthJWT, appointmentController.getAppointmentsByPatientId);
 Appointmentrouter.get('/filter', auth.AuthJWT, appointmentController.filterAppointments);
 Appointmentrouter.put('/status/:id', auth.AuthJWT, appointmentController.changeAppointmentStatus);
-Appointmentrouter.put('/appoint/:id/reschedule', appointmentController.rescheduleAppointment);
-Appointmentrouter.put('/appoint/:id/reply', appointmentController.respondToRescheduleRequest);
+Appointmentrouter.put('/appoint/:id/reschedule', auth.AuthJWT,appointmentController.rescheduleAppointment);
+Appointmentrouter.put('/appoint/:id/reply',auth.BothJWT, appointmentController.respondToRescheduleRequest);
 
 module.exports = Appointmentrouter;
