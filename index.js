ad12ef30adab2d909ce506c1bba078a866083102
iris-credot <tetairiscredot@gmail.com>
2025-05-29 09:13:40 +0200

    Added the users

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/careConnect', swaggerUi.serve, swaggerUi.setup(swagger))
  app.use(cors({
-    origin: ["http://localhost:5173",],
+    origin: ["http://localhost:5173","http://localhost:5174"],
      credentials: true,
      allowedHeaders: ['Content-Type', 'Authorization'],
      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
