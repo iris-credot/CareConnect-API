a2c5525cd5d46a573a27c367efe7d328379e92e2
iris-credot <tetairiscredot@gmail.com>
2025-06-18 14:34:15 +0200

    Added the patient page

--- Routes/sportsRoute.js
+++ Routes/sportsRoute.js
@@ -8,7 +8,7 @@ sportsrouter.get('/all',auth.BothJWT,sportsController.getAllSportRecommendations
 sportsrouter.get('/get/:id',auth.AuthJWT,sportsController.getSportRecommendationById);
 sportsrouter.put('/update/:id',auth.BothJWT,sportsController.updateSportRecommendation);
 sportsrouter.delete('/delete/:id',auth.AuthJWT,sportsController.deleteSportRecommendation);
-sportsrouter.get('/patient/:patientId', auth.BothJWT, sportsController.getSportRecommendationsByPatient);
+sportsrouter.get('/patient/:patientId', auth.AuthJWT, sportsController.getSportRecommendationsByPatient);
 
 
 
